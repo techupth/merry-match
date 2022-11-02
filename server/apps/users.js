96c49a52d7a430d991eee745ae79a3fd14947638
Mind Sathirasrindh <109655871+mydjolie@users.noreply.github.com>
2022-11-03 02:19:45 +0700

    fix:users data add register route

--- server/apps/users.js
+++ server/apps/users.js
@@ -1,19 +1,53 @@
-import Router from 'express';
-import { pool } from '../utils/db.js';
-
-
+import Router from "express";
+import { pool } from "../utils/db.js";
 
 const userRouter = Router();
 
-
-userRouter.get('/', async(req,res)=>{
-    const result = await pool.query(`select * from users`)
-    console.log(result)
-    return res.json({
-        message : "Successful!",
-        data : result.rows,
-    })
+userRouter.get("/", async (req, res) => {
+  const result = await pool.query(`select * from users`);
+  console.log(result);
+  return res.json({
+    message: "Successful!",
+    data: result.rows,
+  });
 });
 
+userRouter.post("/", async (req, res) => {
+  try {
+    const newUserProfile = {
+      ...req.body,
+      created_at: new Date(),
+      updated_at: new Date(),
+    };
+    console.log(newUserProfile);
+    // await pool.query(`SET datestyle = dmy`);
+    await pool.query(
+      `insert into users(name,birthday,location,city,username,email,password,sex_identity,sex_pref,racial_pref,meeting_int,hobby,created_at,updated_at,profile_pics) values($1,$2,$3,$4,$5,$6,$7,$8,$9,$10,$11,$12,$13,$14,$15)`,
+      [
+        newUserProfile.name,
+        newUserProfile.birthday,
+        newUserProfile.location,
+        newUserProfile.city,
+        newUserProfile.username,
+        newUserProfile.email,
+        newUserProfile.password,
+        newUserProfile.sex_identity,
+        newUserProfile.sex_pref,
+        newUserProfile.racial_pref,
+        newUserProfile.meeting_int,
+        newUserProfile.hobby,
+        newUserProfile.created_at,
+        newUserProfile.updated_at,
+        newUserProfile.profile_pics,
+      ]
+    );
+    return res.json({
+      message: "Registered Successful!",
+    });
+  } catch (error) {
+    console.log("error is");
+    console.log(error);
+  }
+});
 
 export default userRouter;
