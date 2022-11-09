4b510fc2bf45adb169ad9de344b0ce70bf9632a8
Piito12 <sitthipong18748@gmail.com>
2022-11-09 11:22:22 +0700

    fix : fix and GET data from database on refeching

--- client/src/components/Navbar.jsx
+++ client/src/components/Navbar.jsx
@@ -8,7 +8,7 @@ const Navbar = () => {
     <nav className="bg-[#FFFFFF] absolute z-10 overflow-auto  flex flex-row items-center justify-between h-[100px] w-full text-[16px] font-bold shadow-[2px_2px_12px_0_rgba(64,50,133,0.12)] ">
       <button
         onClick={() => {
-          navigate("/")
+          navigate("/");
         }}
       >
         <img
--- client/src/pages/AuthHomepage.jsx
+++ client/src/pages/AuthHomepage.jsx
@@ -3,15 +3,11 @@ import React from "react";
 import Home from "./Home";
 import Navbarauthen from "../components/NavbarAuthen";
 
-
-
-
 const HomepageAuth = () => {
   return (
     <div>
       <Navbarauthen />
       <Home />
-      
     </div>
   );
 };
