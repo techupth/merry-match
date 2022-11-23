4da6eb91e3db060696eb0b9203cc63ec4a44201b
Vasin <bestwasin1617@gmail.com>
2022-11-23 15:26:23 +0700

    fix:design hompage

--- client/src/components/ladingPage/Header.jsx
+++ client/src/components/ladingPage/Header.jsx
@@ -6,7 +6,7 @@ function Header() {
   return (
     <div className="z-0">
       <div className="w-[100%] h-[100vh] bg-[url('../../../public/asset/header/hero-section-onlybg.svg')]   bg-no-repeat bg-cover z-0 overflow-auto xl:bg-[url('../../../public/asset/header/hero-section-crop.svg')]">
-        <div className="h-[100%]w-[100%] flex flex-col items-center justify-center ">
+        <div className="h-[100%] w-[100%] flex flex-col items-center justify-center   ">
           <div className="flex flex-col items-center justify-start h-[60%] ">
             <h3 className="text-[400%] text-[#FFFFFF] font-[900] leading-[115%] mt-[70%] ">
               Make the
@@ -22,7 +22,7 @@ function Header() {
           </div>
 
           <button
-            className="button-header bg-[#c70039] shadow-[2px_2px_12px_0_rgba(64, 50, 133, 0.16)] rounded-[99px] text-[#ffffff] h-[5%] w-[15%]  font-[700] xl:mt-[5%] hover:-translate-y-1 hover:scale-110 hover:bg-pink-300 duration-300"
+            className="button-header bg-[#c70039] shadow-[2px_2px_12px_0_rgba(64, 50, 133, 0.16)] rounded-[99px] text-[#ffffff] h-[6%] w-[9%] mt-[5%] font-[700] hover:-translate-y-1 hover:scale-110 hover:bg-pink-300 duration-300"
             onClick={() => {
               navigate("/login");
             }}
