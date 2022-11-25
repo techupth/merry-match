f146e1b5b4714166c3514384a9559f66d79dfbce
ingpawat / Y <ingpawat.k@gmail.com>
2022-11-25 11:56:34 +0700

    feat: layout

--- client/src/pages/userComplaintPage/userComplaintPage.jsx
+++ client/src/pages/userComplaintPage/userComplaintPage.jsx
@@ -1,10 +1,27 @@
 import React from "react";
 import Banner from "../../../public/asset/Login/bannerLogin.png";
+import Footer from "../../components/editPageComponents/Footer";
 
 const UserComplaintPage = () => {
   return (
-    <div className="w-[full] h-[80.25rem] bg-[white]">
-      <img src={Banner} alt="banner" />
+    <div className="w-[full] h-[70.25rem] bg-[white] flex flex-col">
+      <div className="flex flex-row">
+
+        {/* ********************************** Content *************************************** */}
+        <div className="w-[65%] h-[50rem] border border-3 border-red-600 mt-[10%] ml-[5%]">
+            
+        </div>
+
+        {/* ********************************** Banner *************************************** */}
+        <img
+          src={Banner}
+          alt="banner"
+          className="w-[28.125rem] h-[42.313rem] mt-[10%]"
+        />
+      </div>
+
+      {/* ********************************** Footer *************************************** */}
+      <Footer />
     </div>
   );
 };
