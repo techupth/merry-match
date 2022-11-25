da4a48029dd37379a5e4e88f4c38d57e7d7f219b
Mind Sathirasrindh <109655871+mydjolie@users.noreply.github.com>
2022-11-25 17:25:48 +0700

    feat:set up adminPanelControlPage

--- client/src/pages/router/Unauthenticated.jsx
+++ client/src/pages/router/Unauthenticated.jsx
@@ -3,6 +3,7 @@ import Navbar from "../../components/Navbar/Navbar";
 import Home from "../landingPage/Home";
 import Login from "../loginPage/Login";
 import Register from "../registerPage/Register";
+import AdminPanelControlPage from "../adminPanelControlPage/adminPanelControlPage";
 import SwipeModal from "../../components/matchingComponents/swipeModal";
 
 const Unauthenticated = () => {
@@ -14,6 +15,8 @@ const Unauthenticated = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route path="*" element={<Home />} />
+        {/* อย่าลืมลบ ↓ */}
+        <Route path="/dev" element={<AdminPanelControlPage />} />
       </Routes>
     </>
   );
