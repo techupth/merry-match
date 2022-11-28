7314ab2d6b3171cf50b1ffb72054321193f5d67a
ingpawat / Y <ingpawat.k@gmail.com>
2022-11-28 14:25:31 +0700

    archive: pop-up

--- client/src/components/matchingComponents/PopupWhenSwipe.jsx
+++ client/src/components/matchingComponents/PopupWhenSwipe.jsx
@@ -5,7 +5,6 @@ const PopupWhenSwipe = ({ close }) => {
     <div className="bg-[rgba(49,49,49,0.8);] w-[2500px] h-[2500px] flex justify-center absolute z-10 ">
       <button className="animate-pulse absolute mt-[31.25rem] font-[700] w-[239px] h-[70px] rounded-md bg-[#FFE1EA] text-[#95002B] text-[20px]">
         <p>You just swiped!</p>
-        {/* <p className="text-[15px]">Look up your Merry list</p> */}
       </button>
     </div>
   );
